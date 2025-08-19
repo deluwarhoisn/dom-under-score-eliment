96f694da001a78513fcf3bb13bd11b3fa6fbbde7
deluwarhoisn <babuhossen301@gmail.com>
2025-08-19 21:05:27 +0600

    btn bg color event handaling

--- ja/events.js
+++ ja/events.js
@@ -1,1 +1,24 @@
 console.log('extranal file')
+
+// option 2 to handle event
+  function makeYellow(){
+            document.body.style.backgroundColor='yellow'
+        }
+        function makered(){
+            document.body.style.backgroundColor='red'
+        }
+
+        // option 3: get Element by Id and then set onclick 
+          const btnMakeBlue =document.getElementById('btn-make-blue');
+    //   console.log(btnMakeBlue);
+    btnMakeBlue.onclick = function makeBlue(){
+        document.body.style.backgroundColor='blue'
+    }
+
+    // option 3: differents varsun
+    const btnmakepurple = document.getElementById('btn-make-purple');
+    // console.log(btnmakepurple);
+        btnmakepurple.onclick=makepurple;
+    function makepurple (){
+        document.body.style.backgroundColor='purple'
+    }
